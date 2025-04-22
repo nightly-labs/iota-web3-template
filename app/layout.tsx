1275ac68c7f840577e1a9ac7ac1c5558ea9e94c3
LukassF <szkulkerolf@gmail.com>
2025-04-22 10:27:43 +0200

    try fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content="IOTA Template App" />
+        <meta
+          property="og:description"
+          content="Start your IOTA journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet."
+        />
+        <meta
+          property="og:image"
+          content="https://iota-web3-template.nightly.app/preview.png?v2"
+        />
+        <meta
+          property="og:url"
+          content="https://iota-web3-template.nightly.app"
+        />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@nightly_app" />
+        <meta name="twitter:title" content="IOTA Template App" />
+        <meta
+          name="twitter:description"
+          content="Start your IOTA journey here, without unnecessary configuration and setup. Just clone it and code on top of it. Powered by Nightly Wallet."
+        />
+        <meta
+          name="twitter:image"
+          content="https://iota-web3-template.nightly.app/preview.png?v2"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
